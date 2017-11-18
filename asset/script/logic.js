d9532c0b027a8988d28ddd7a699a70ac062a9972
unknown <mtoasis22@gmail.com>
2017-11-18 00:44:15 -0800

    function finished

--- asset/script/logic.js
+++ asset/script/logic.js
@@ -10,74 +10,105 @@
   firebase.initializeApp(config);
   var database = firebase.database();
   var ref = database.ref();
-  var name="";  
-  var player_number=0;
-  var index=0;
+  var player_number;
   var flag=true;
-  var result_on = false;
+  var name ="";
+  var winner;
 
 
-//   var connectionsRef = database.ref("/connections");
-//   var connectedRef = database.ref(".info/connected");
+  var connectionsRef = database.ref("Players");
+  var connectedRef = database.ref(".info/connected");
 
-//   connectedRef.on("value", function(snap) {
+  connectedRef.once("value", function(snap) {  
 
-//   if (snap.val()) {
+    ref.child("Players").on("value",function(snap){ 
+    
+    if (snap.numChildren()==0){
+      ref.child("Messages").remove();
+    }
+  })
 
-//     var con = connectionsRef.push(true);
-//     con.onDisconnect().remove();
-//   }
-// });
+     $("#start_button").on("click",function(){
+
+        if (flag){
+          flag =false;
+
+        name = $("#user_name").val().trim();        
+
+        var new_player = {
+          name: name,
+          wins: 0,
+          losses: 0,
+          choice: "undecided"
+            }
+
+        ref.child("Players").once("value",function(snap){
 
 
-database.ref().once("value",function(snapshot){
+          if (snap.numChildren()<=2){
 
-  if (!snapshot.hasChild("Player1") && !snapshot.hasChild("Player2")){
-    database.ref().child("Messages").set("Welcome to RPS Game!");
+            if (snap.numChildren()==0 || snap.hasChild("Player2")){
+            ref.child("Players").child("Player1").set(new_player);
+            player_number = 1;
 
-  }
+
+            }
+            
+            else if (snap.hasChild("Player1")){
+            ref.child("Players").child("Player2").set(new_player);
+            player_number = 2;
+
+            }
+              database.ref().child("Messages").push("<b>"+name+"</b> entered the game");
+              display()
+              console.log("mynumberis :"+player_number)
+
+            }
+
+          if (player_number ==1){
+                  ref.child("Players").child("Player1").onDisconnect().remove();                  
+                }
+          else if (player_number ==2){
+                  ref.child("Players").child("Player2").onDisconnect().remove();
+          }
+
+             $(document).on("click",".choice", function(){
+
+                var choice = {
+                choice: $(this).text()};
+                var choice_text = $(this).text();
+
+                ref.child("Players").child("Player"+player_number).update(choice);
+                var choice_div = $(".player_"+player_number+"_ready_div");
+                choice_div.text("Your choice: "+choice_text)
+
+              })
+
+             if (player_number==1){
+             ref.child("Players").child("Player2").on("value", function(snap){
+              var isReady = snap.val().choice;
+              if (isReady!="undecided"){
+                $(".player_2_ready_div").text("READY");
+              }
+            })
+            }
+           else if (player_number==2){
+            ref.child("Players").child("Player1").on("value", function(snap){
+              var isReady = snap.val().choice;
+                if (isReady!="undecided"){
+                $(".player_1_ready_div").text("READY");
+              }
+           })
+          }
+
+
+
+
+      })
+      }        
+  })
+
 });
-
-
- $("#start_button").on("click",function(){ 
-
-    ref.once('value',function(snapshot) {
-
-
-     index = (Object.keys(snapshot.val()).length);
-
-    });    
-
-  if (index>0 && index<3 && flag){
-  name = $("#user_name").val().trim();
-  database.ref().child("Messages").push("<b>"+name+"</b> entered the game");
-
- 	var new_player = {
- 		name: name,
- 		wins: 0,
- 		losses: 0,
-    choice: "undecided"
- 		 	}
-
- 	ref.child("Player"+index).update(new_player);
-  player_number = index;
-
-  display();
-  console.log("player number: "+player_number)
-  flag = false;
-  } 
-  }) 	
-
-
-
- $(document).on("click",".choice", function(){
-
- 	var choice = {
- 		choice: $(this).text()};
-
- 	ref.child("Player"+index).update(choice);
-
- })
 
 function display(){
 
@@ -90,11 +121,12 @@ function display(){
   $(".player_"+player_number+"_div").append(choice_scissors);
 }
 
-  ref.on('value',function(snapshot){
+
+  ref.child("Players").on('value',function(snapshot){
 
     if (snapshot.hasChild("Player1") || snapshot.hasChild("Player2")){
    
-  var index = Object.keys(snapshot.val()).length;
+    var index = snapshot.numChildren();
 
     if (snapshot.hasChild("Player1")){    
     var name_1 = snapshot.val().Player1.name;    
@@ -104,94 +136,24 @@ function display(){
     if (snapshot.hasChild("Player2")){
     var name_2 = snapshot.val().Player2.name;
     $(".player_2_div").children(".player_name").text(name_2);
-  }
-}
-    
+       }
+    }    
   })
 
 
-  ref.on('value',function(snapshot){ //operation
-    $(".winner_div").text("Waiting for Players");
+  ref.child("Players").on("value", function(snapshot){
+    var i=1;
+    snapshot.forEach(function(childsnap){
+      var win_stat = childsnap.val().wins;
+      var lose_stat = childsnap.val().losses;
+      var stat_div = ".stat_"+i+"_div"
 
-    var winner = "";
-
-    if (snapshot.hasChild("Player1") && snapshot.hasChild("Player2")){
-    var name_1 = snapshot.val().Player1.name;
-    var name_2 = snapshot.val().Player2.name;
-    var player_1_choice = snapshot.val().Player1.choice;
-    var player_2_choice = snapshot.val().Player2.choice;
-
-  
-  if (player_1_choice=="undecided" || player_2_choice=="undecided"){
-
-    winner = "Waiting..."
-  }
-
-  else if (player_1_choice=="Rock" && player_2_choice=="Rock"){
-
-    winner = "Tie";
-    initialize();
-
-  }
-  else if (player_1_choice=="Scissors" && player_2_choice=="Scissors"){
-
-     winner = "Tie";
-     initialize();
-  }
-  else if (player_1_choice=="Paper" && player_2_choice=="Paper"){
-
-     winner = "Tie";
-     initialize();
-  }
-  else if (player_1_choice=="Rock" && player_2_choice =="Scissors"){
-
-    winner = name_1;
-    initialize();
-
-  }
-  else if (player_1_choice=="Rock" && player_2_choice =="Paper"){
-
-    winner = name_2;
-    initialize();
-  }
-  else if (player_1_choice=="Scissors" && player_2_choice =="Paper"){
-
-    winner = name_1;
-    initialize();
-  }
-  else if (player_1_choice=="Scissors" && player_2_choice =="Rock"){
-
-    winner = name_2;
-    initialize();
-  }
-  else if (player_1_choice=="Paper" && player_2_choice =="Rock"){
-
-    winner = name_1;
-    initialize();
-  }
-  else if (player_1_choice=="Paper" && player_2_choice =="Scissors"){
-
-    winner = name_2;
-    initialize();
-  }
-
-     $(".winner_div").text("Winner is: "+winner);
- 
-}
-})
+      $(stat_div).text("wins: "+win_stat+"   "+"losses: "+lose_stat);
+      i+=1;
+    })
+  })
 
 
-
-
-function initialize(){
-    var choice = {
-    choice: "undecided"
-  };
-
-  ref.child("Player1").update(choice);
-  ref.child("Player2").update(choice);
-  
-};
 
 
 
@@ -199,7 +161,7 @@ $("#message_input_button").on("click",function(){
 
   var message = $("#message_input").val();
   ref.child("Messages").push("<b>"+name+"</b>"+" : "+message);
-  $("#message_input").val() = "";
+  $("#message_input").val("");
 })
 
 
@@ -212,6 +174,131 @@ database.ref().child("Messages").on("child_added",function(snapshot){
 })
 
 
-database.ref().child("Connection").onDisconnect().remove();
+
+  ref.child("Players").on('value',function(snapshot){ //operation
+    $(".winner_div").text("Waiting for Players");
+
+    var winner = "";
+
+    if (snapshot.hasChild("Player1") && snapshot.hasChild("Player2")){
+    var name_1 = snapshot.val().Player1.name;
+    var name_2 = snapshot.val().Player2.name;
+    var player_1_choice = snapshot.val().Player1.choice;
+    var player_2_choice = snapshot.val().Player2.choice;
+
+  
+  if (player_1_choice=="undecided" || player_2_choice=="undecided"){
+  }
+
+  else if (player_1_choice=="Rock" && player_2_choice=="Rock"){
+
+    winner = "Tie";
+    
+
+  }
+  else if (player_1_choice=="Scissors" && player_2_choice=="Scissors"){
+
+     winner = "Tie";
+     
+  }
+  else if (player_1_choice=="Paper" && player_2_choice=="Paper"){
+
+     winner = "Tie";
+     
+  }
+  else if (player_1_choice=="Rock" && player_2_choice =="Scissors"){
+
+    winner = name_1;
+    
+
+  }
+  else if (player_1_choice=="Rock" && player_2_choice =="Paper"){
+
+    winner = name_2;
+    
+  }
+  else if (player_1_choice=="Scissors" && player_2_choice =="Paper"){
+
+    winner = name_1;
+    
+  }
+  else if (player_1_choice=="Scissors" && player_2_choice =="Rock"){
+
+    winner = name_2;
+    
+  }
+  else if (player_1_choice=="Paper" && player_2_choice =="Rock"){
+
+    winner = name_1;
+    
+  }
+  else if (player_1_choice=="Paper" && player_2_choice =="Scissors"){
+
+    winner = name_2;
+    
+  }    
+
+  var win_p;
+  var lose_p;
+
+  if (winner == name_1){
+    win_p = "Player1";
+    lose_p = "Player2";
+    initialize();
+    remove_ready();
+
+  }
+  else if (winner ==name_2){
+     win_p = "Player2";
+    lose_p = "Player1";
+    initialize();
+    remove_ready(); 
+    }
+  else if (winner =="Tie"){
+    initialize();
+    remove_ready(); 
+  }
+
+  if(winner !=""){
+    ref.child("Players").child(win_p).once("value",function(snap){
+      var wins = {wins: snap.val().wins+1};
+      ref.child("Players").child(win_p).update(wins)
+    })
+
+    ref.child("Players").child(lose_p).once("value",function(snap){
+      var losses = {losses: snap.val().losses+1}
+      ref.child("Players").child(lose_p).update(losses)
+    })
+
+    $(".winner_div").text("Winner is: "+winner);
+
+
+  }
+
+  else{
+    $(".winner_div").text("Waiting...");
+  }
+
+}
+})
+
+
+function initialize(){
+    var choice = {
+    choice: "undecided"
+  };
+
+  ref.child("Players").child("Player1").update(choice);
+  ref.child("Players").child("Player2").update(choice);  
+};
+
+function remove_ready(){
+    $(".player_1_ready_div").remove()
+    $(".player_2_ready_div").remove()
+    $(".game_display_div").append('<div class="player_1_ready_div">')
+    $(".game_display_div").append('<div class="player_2_ready_div">')
+}
+
+
 
 
